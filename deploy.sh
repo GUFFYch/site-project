93477511276f35ca7986b847c56a89966cc1eab7
DIMMy <dimmy@pop-os.localdomain>
2023-07-16 17:39:51 +0300

    Deploying app

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:GUFFYch/site-project.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/site-project/',
   transpileDependencies: [
     'vuetify'
   ]
 })
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === "production" ? "/site-project/" : "/",
+// };
