799a588964d7e3400c2c98cd0b61511ef3d0a5fa
GUFFYch <koromit15@gmail.com>
2023-07-31 19:51:59 +0300

    deployed version

--- deploy.sh
+++ deploy.sh
@@ -9,12 +9,18 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-
+# initialize a new temporary Git repository
 git init
+
+# add all files to the new repository
 git add -A
+
+# commit the changes
 git commit -m 'deploy'
 
-# if deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:GUFFYch/site-project.git main:gh-pages
+# push the changes to the gh-pages branch on GitHub
+git push -f git@github.com:GUFFYch/site-project.git HEAD:gh-pages
 
-cd -
+# clean up
+cd ..
+rm -rf dist
